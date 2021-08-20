01da79e270fa6c016112c6732005b7e2b439cb91
duc.doan <3dmailboxes@gmail.com>
2021-08-19 23:24:27 -0400

    add project files

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+import {
+  BrowserRouter as Router, Link,
+} from "react-router-dom";
+import 'antd/dist/antd.css'
+import {Layout, Menu} from 'antd';
+import './App.css';
+import React, {useState} from "react";
+import {LoggedInContent} from "./components/LoggedInContent";
+import {LoggedoutContent} from "./components/LoggedoutContent";
+
+const { Header, Content } = Layout;
+
+function App() {
+  const [user, setUser] = useState(localStorage.getItem("currUser") ? JSON.parse(localStorage.getItem("currUser")) : {
+    id: 0,
+    name: ""
+  })
+
+  const setLocalUser = (user) => {
+    localStorage.setItem("currUser", JSON.stringify(user))
+    setUser(user)
+  }
+
+  const logOut = () => {
+    localStorage.removeItem("currUser");
+    setUser({
+      id: 0,
+      name: ""
+    })
+  }
+
+  const onHeaderClick = ({key}) => {
+    if (key === "logout") logOut()
+  }
+
+  return (
+    <Router>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header>
+          <Menu theme="dark" mode="horizontal" onClick={onHeaderClick}>
+            {
+              user.id === 0 ? (
+                <Menu.Item key="welcome"><Link to="/home">Welcome to STEAM</Link></Menu.Item>
+              ) : (
+                <>
+                  <Menu.Item key="welcome"><Link to="/home">Welcome to STEAM, {user.name}</Link></Menu.Item>
+                  <Menu.Item key="logout">Log out</Menu.Item>
+                </>
+              )
+            }
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          {
+            user.id === 0 ? <LoggedoutContent onUserChange={setLocalUser}/> : <LoggedInContent/>
+          }
+        </Content>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
